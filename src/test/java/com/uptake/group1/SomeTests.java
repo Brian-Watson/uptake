b935451a4446294dc3de2066ad08fb259238cc09
brian.watson <brian.watson@docusign.com>
2016-08-28 16:04:19 -0500

    Add groups

---
+++ src/test/java/com/uptake/group1/SomeTests.java
@@ -0,0 +1,19 @@
+package com.uptake.group1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SomeTests {
+
+    @Test
+    public void testPass() {
+        assertEquals("Some test", true, true);
+    }
+
+    @Test
+    public void testPass2() {
+        assertEquals("Some test", "x", "x");
+    }
+
+
+}
